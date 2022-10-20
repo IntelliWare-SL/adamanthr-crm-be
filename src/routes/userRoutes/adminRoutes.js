0d544a68c8195a0f536d8c8d789e39a6fc166435
mahela97 <m.pradeepd@gmail.com>
2022-10-20 21:16:26 +0530

    done token

--- src/routes/userRoutes/adminRoutes.js
+++ src/routes/userRoutes/adminRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userController from "../../controllers/userController";
+import checkToken from "../../middleware/checkAuth";
+import isAdmin from "../../middleware/isAdmin";
 const router = express.Router();
 
-router.route("/registerEmployee").post(userController.registerUser)
+router.route("/registerEmployee").post(checkToken, isAdmin,userController.registerUser)
 router.route("/login").post(userController.loginUser)
 
 export default router;
